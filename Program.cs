087457c9eebd494fb482b41c70d62ac3f16ed8ad
Drew Meseck <dm3306a@student.american.edu>
2019-09-12 12:47:30 -0400

    Added functon/subfunction structure for main algorithm.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MultiplyPolynomials
 {
@@ -6,7 +7,19 @@ namespace MultiplyPolynomials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        static int[] Multiply(int[] p1, int[] p2) //The Algorithm
+        {
+            int[] p3 = { };
+            return p3;
+        }
+
+        static int[] AddMerge(LinkedList<Tuple<int>> exp)//A subfunction that is part of the 
+        {
+            int[] NewPoly = { };
+            return NewPoly;
         }
     }
 }
