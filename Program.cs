ed101dc9235658c8117079df826c89cbdf3706fb
Drew Meseck <dm3306a@student.american.edu>
2019-09-16 16:37:38 -0400

    Basic function logic, no checks yet

--- Program.cs
+++ Program.cs
@@ -5,21 +5,71 @@ namespace MultiplyPolynomials
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Give input and run the Alogorithm
         {
-            
+            int[] p1 = { };
+            int[] p2 = { };
+
+            int[] result = Multiply(p1, p2);
         }
 
         static int[] Multiply(int[] p1, int[] p2) //The Algorithm
         {
-            int[] p3 = { };
-            return p3;
+
+            List<Node> temp = new List<Node>();
+                
+            for (int i = 1; i < p1[1]; i += 2)
+            {
+                for(int j = 1; j < p2[1]; j += 2)
+                {
+                    //What happens if one of the lists is empty??
+                    Node node = new Node(p1[i] + p2[j], p1[i+1] * p2[j+1]);
+                    temp.Add(node);
+                }
+            }
+            
+            return AddMerge(temp);
         }
 
-        static int[] AddMerge(LinkedList<Tuple<int>> exp)//A subfunction that is part of the 
+        static int[] AddMerge(List<Node> node)//A subfunction that is part of the algorithm, adds like terms and outputs the final result
         {
-            int[] NewPoly = { };
+            for (int i = 0; i < node.Count; i++)
+            {
+                for (int j = 1; j < node.Count; j ++)
+                {
+                    if (node[i].exp == node[j].exp)
+                    {
+                        node[i].coef += node[j].coef;
+                        node.RemoveAt(j);
+                    }
+                }
+            }
+
+            int[] NewPoly = new int[node.Count*2 + 1];
+            NewPoly[0] = node.Count;
+            int f = 1;
+            for(int i = 1; i < node.Count; i++)
+            {
+                NewPoly[f] = node[i].exp;
+                NewPoly[f + 1] = node[i + 1].coef;
+                f += 2;
+            }
             return NewPoly;
         }
+
+
+    }
+
+    class Node
+    {
+        public int exp { get; set; }
+        public int coef { get; set; }
+
+        public Node(int e, int c)
+        {
+            this.exp = e;
+            this.coef = c;
+        }
+
     }
 }
